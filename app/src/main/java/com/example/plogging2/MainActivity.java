16e991ac4874d016aa9ab7139589686a58122583
1GameGate <bravofund1@gmail.com>
2023-05-23 23:20:08 +0900

    Data save/load

--- app/src/main/java/com/example/plogging2/MainActivity.java
+++ app/src/main/java/com/example/plogging2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -15,7 +16,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.File;
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     TextView textViewStepCounter, textViewWasteCounter;
@@ -24,6 +32,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     File file;
     boolean run = false, needToReset = false, isExercising = false;
     int stepCount = 0, wasteCount = 0;
+
+    ArrayList<String> data_Date = new ArrayList<>();
+    ArrayList<String> data_stepCount = new ArrayList<>();
+    ArrayList<String> data_wasteCount = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -151,5 +163,77 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         wasteCount--;
         if(wasteCount < 0) wasteCount = 0;
         textViewWasteCounter.setText(String.valueOf(wasteCount));
+    }
+
+    public void dataSave(View v)
+    {
+        long mNow = System.currentTimeMillis();
+        Date mDate = new Date(mNow);
+        SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String currDate = mFormat.format(mDate);
+
+        data_Date.add(currDate);
+        data_stepCount.add(textViewStepCounter.getText().toString());
+        data_wasteCount.add(textViewWasteCounter.getText().toString());
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("PloggingData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        Gson gson = new Gson();
+
+        String json1 = gson.toJson(data_Date);
+        String json2 = gson.toJson(data_stepCount);
+        String json3 = gson.toJson(data_wasteCount);
+        editor.putString("data_Date", json1);
+        editor.putString("data_stepCount", json2);
+        editor.putString("data_wasteCount", json3);
+        editor.apply();
+    }
+
+    public void dataLoad(View v)
+    {
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("PloggingData", MODE_PRIVATE);
+
+        Gson gson = new Gson();
+        String json1 = sharedPreferences.getString("data_Date", null);
+        String json2 = sharedPreferences.getString("data_stepCount", null);
+        String json3 = sharedPreferences.getString("data_wasteCount", null);
+
+        Type type = new TypeToken<ArrayList<String>>(){}.getType();
+
+        data_Date = gson.fromJson(json1,type);
+        data_stepCount = gson.fromJson(json2,type);
+        data_wasteCount = gson.fromJson(json3,type);
+
+        if(data_Date == null) data_Date = new ArrayList<String>();
+        if(data_stepCount == null) data_stepCount = new ArrayList<String>();
+        if(data_wasteCount == null) data_wasteCount = new ArrayList<String>();
+
+        TextView textViewDebug = findViewById(R.id.textViewdebugText);
+        String temp = "";
+        for(int i = 0; i < data_Date.size(); i++)
+        {
+            temp += data_Date.get(i) + " " + data_stepCount.get(i) + " " + data_wasteCount.get(i) + "\n";
+        }
+        textViewDebug.setText(temp);
+    }
+
+    public void initializeData(View v)
+    {
+        Gson gson = new Gson();
+        data_Date = new ArrayList<String>();
+        data_stepCount = new ArrayList<String>();
+        data_wasteCount = new ArrayList<String>();
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("PloggingData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String json1 = gson.toJson(data_Date);
+        String json2 = gson.toJson(data_stepCount);
+        String json3 = gson.toJson(data_wasteCount);
+        editor.putString("data_Date", json1);
+        editor.putString("data_stepCount", json2);
+        editor.putString("data_wasteCount", json3);
+        editor.apply();
     }
 }
